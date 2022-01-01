c85ea64d10b4e3e130755270035b832d1187462b
Aditya Kumar <adi6@umbc.edu>
2022-01-01 17:30:03 -0500

    added about section in toolbar of app

--- app/src/main/java/com/raywenderlich/AdityaKumar/timefighter/MainActivity.kt
+++ app/src/main/java/com/raywenderlich/AdityaKumar/timefighter/MainActivity.kt
@@ -1,14 +1,18 @@
 package com.raywenderlich.AdityaKumar.timefighter
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
 import android.os.PersistableBundle
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import android.view.animation.AnimationUtils
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 
 class MainActivity : AppCompatActivity() {
 
@@ -50,6 +54,30 @@ class MainActivity : AppCompatActivity() {
             val blinkAnim = AnimationUtils.loadAnimation(this, R.anim.blink)
             gameScoreTextView.startAnimation(blinkAnim)
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        super.onCreateOptionsMenu(menu)
+        menuInflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.actionAbout){
+            showInfo()
+        }
+        return true
+    }
+
+    private fun showInfo(){
+        val dialogTitle = getString(R.string.aboutTitle)
+        val dialogMsg = getString(R.string.aboutMsg)
+
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle(dialogTitle)
+        builder.setMessage(dialogMsg)
+        builder.create().show()
+
     }
 
     private fun restoreGame() {
